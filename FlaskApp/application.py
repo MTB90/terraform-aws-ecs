c3c7aa483a6cf7f376ea54c04e439b6c8812b485
Mateusz Bojanowski <mbojanowski@pgs-soft.com>
2018-06-18 10:45:05 +0200

    Fix flake8 for FlaskApp/aplication.py.

--- FlaskApp/application.py
+++ FlaskApp/application.py
@@ -1,28 +1,34 @@
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
-# in compliance with the License. A copy of the License is located at
-#
-# https://aws.amazon.com/apache-2-0/
-#
-# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-"Demo Flask application"
+"""
+Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+https://aws.amazon.com/apache-2-0/
+or in the "license" file accompanying this file.
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing
+permissions and limitations under the License.
+"""
+
 import sys
 from datetime import datetime
 
 import requests
-from requests.auth import HTTPBasicAuth
 import boto3
-from flask import Flask, render_template_string, session, redirect, request, url_for
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
-from wtforms import TextAreaField
 import flask_login
-from jose import jwt
+
+from requests.auth import HTTPBasicAuth
+from wtforms import TextAreaField
+from flask import Flask, render_template_string, session
+from flask import redirect, request, url_for, render_template
+from flask_wtf import FlaskForm
+
+from flask_wtf.file import FileField, FileRequired
 from aws_xray_sdk.core import xray_recorder, patch_all
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+from jose import jwt
 
 import config
 import util
@@ -30,23 +36,24 @@ import database
 
 application = Flask(__name__)
 application.secret_key = config.FLASK_SECRET
-
 login_manager = flask_login.LoginManager()
 login_manager.init_app(application)
 
-### load and cache cognito JSON Web Key (JWK)
+
+# load and cache cognito JSON Web Key (JWK)
 # https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
 JWKS_URL = ("https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json"
             % (config.AWS_REGION, config.COGNITO_POOL_ID))
 JWKS = requests.get(JWKS_URL).json()["keys"]
 
-### x-ray set up
+# x-ray set up
 plugins = ('EC2Plugin',)
 xray_recorder.configure(service='MyApplication', plugins=plugins)
 XRayMiddleware(application, xray_recorder)
 patch_all()
 
-### FlaskForm set up
+
+# FlaskForm set up
 class PhotoForm(FlaskForm):
     """flask_wtf form class the file upload"""
     photo = FileField('image', validators=[
@@ -54,9 +61,11 @@ class PhotoForm(FlaskForm):
     ])
     description = TextAreaField(u'Image Description')
 
+
 class User(flask_login.UserMixin):
     """Standard flask_login UserMixin"""
     pass
+
 
 @login_manager.user_loader
 def user_loader(session_token):
@@ -74,6 +83,7 @@ def user_loader(session_token):
     user.nickname = session['nickname']
     return user
 
+
 @application.route("/")
 def home():
     """Homepage route"""
@@ -86,6 +96,7 @@ def home():
         Click <em>login in / sign up<em> to access this site.
         {% endif %}
         {% endblock %}""")
+
 
 @application.route("/myphotos", methods=('GET', 'POST'))
 @flask_login.login_required
@@ -136,62 +147,19 @@ def myphotos():
             # added user id and description to the database
             #######
             labels_comma_separated = ", ".join(all_labels)
-            database.add_photo(key, labels_comma_separated,
-                               form.description.data, flask_login.current_user.id)
+            database.add_photo(key,
+                               labels_comma_separated,
+                               form.description.data,
+                               flask_login.current_user.id)
+
             form.description.data = ''
 
-    return render_template_string("""
-            {% extends "main.html" %}
-            {% block content %}
-            <h4>Upload Photo</h4>
-            <form method="POST" enctype="multipart/form-data" action="{{ url_for('myphotos') }}">
-                {{ form.csrf_token }}
-                  <div class="control-group">
-                   <label class="control-label">Photo</label>
-                    {{ form.photo() }}
-                  </div>
-                  <div class="control-group">
-                    <label class="control-label">Description</label>
-                    <div class="controls">
-                    {{ form.description(class="form-control") }}
-                    </div>
-                  </div>
-                    &nbsp;
-                   <div class="control-group">
-                    <div class="controls">
-                        <input class="btn btn-primary" type="submit" value="Upload">
-                    </div>
-                  </div>
-            </form>
+    return render_template("myphotos.html",
+                           form=form,
+                           url=url,
+                           photos=photos,
+                           all_labels=all_labels)
 
-            {% if url %}
-            <hr/>
-            <h3>Uploaded!</h3>
-            <img src="{{url}}" /><br/>
-            {% for label in all_labels %}
-            <span class="label label-info">{{label}}</span>
-            {% endfor %}
-            {% endif %}
-            
-            {% if photos %}
-            <hr/>
-            <h4>Photos</h4>
-            {% for photo in photos %}
-                <table class="table table-bordered">
-                <tr> <td rowspan="4" class="col-md-2 text-center"><img width="150" src="{{photo.signed_url}}" />
-                    <a href="{{ url_for('myphotos_delete', object_key=photo.object_key) }}"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> delete</a>
-                </td></tr>
-                <tr> <th scope="row" class="col-md-2">Description</th> <td>{{photo.description}}</td> </tr>
-                <tr> <th scope="row" class="col-md-2">Labels</th> <td>{{photo.labels}}</td> </tr>
-                <tr> <th scope="row" class="col-md-2">Created</th> <td>{{photo.created_datetime}} UTC</td> </tr>
-                </table>
-
-            {% endfor %}
-            {% endif %}
-
-
-            {% endblock %}
-                """, form=form, url=url, photos=photos, all_labels=all_labels)
 
 @application.route("/myphotos/delete/<path:object_key>")
 @flask_login.login_required
@@ -200,14 +168,17 @@ def myphotos_delete(object_key):
     database.delete_photo(object_key, flask_login.current_user.id)
     return redirect(url_for("myphotos"))
 
+
 @application.route("/info")
 def info():
-    "Webserver info route"
+    """Webserver info route"""
+
     metadata = "http://169.254.169.254"
-    instance_id = requests.get(metadata +
-                               "/latest/meta-data/instance-id").text
-    availability_zone = requests.get(metadata +
-                                     "/latest/meta-data/placement/availability-zone").text
+    instance_id = requests.get(
+        metadata + "/latest/meta-data/instance-id").text
+
+    availability_zone = requests.get(
+        metadata + "/latest/meta-data/placement/availability-zone").text
 
     return render_template_string("""
             {% extends "main.html" %}
@@ -220,6 +191,7 @@ def info():
                                   availability_zone=availability_zone,
                                   sys_version=sys.version)
 
+
 @application.route("/login")
 def login():
     """Login route"""
@@ -229,9 +201,12 @@ def login():
                      "login?response_type=code&client_id=%s"
                      "&state=%s"
                      "&redirect_uri=%s/callback" %
-                     (config.COGNITO_DOMAIN, config.COGNITO_CLIENT_ID, session['csrf_state'],
+                     (config.COGNITO_DOMAIN,
+                      config.COGNITO_CLIENT_ID,
+                      session['csrf_state'],
                       config.BASE_URL))
     return redirect(cognito_login)
+
 
 @application.route("/logout")
 def logout():
@@ -240,20 +215,23 @@ def logout():
     flask_login.logout_user()
     cognito_logout = ("https://%s/"
                       "logout?response_type=code&client_id=%s"
-                      "&logout_uri=%s/" %
-                      (config.COGNITO_DOMAIN, config.COGNITO_CLIENT_ID, config.BASE_URL))
+                      "&logout_uri=%s/" % (config.COGNITO_DOMAIN,
+                                           config.COGNITO_CLIENT_ID,
+                                           config.BASE_URL))
+
     return redirect(cognito_logout)
+
 
 @application.route("/callback")
 def callback():
     """Exchange the 'code' for Cognito tokens"""
-    #http://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
+    # http://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
     csrf_state = request.args.get('state')
     code = request.args.get('code')
     request_parameters = {'grant_type': 'authorization_code',
                           'client_id': config.COGNITO_CLIENT_ID,
                           'code': code,
-                          "redirect_uri" : config.BASE_URL + "/callback"}
+                          "redirect_uri": config.BASE_URL + "/callback"}
     response = requests.post("https://%s/oauth2/token" % config.COGNITO_DOMAIN,
                              data=request_parameters,
                              auth=HTTPBasicAuth(config.COGNITO_CLIENT_ID,
@@ -261,9 +239,11 @@ def callback():
 
     # the response:
     # http://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
-    if response.status_code == requests.codes.ok and csrf_state == session['csrf_state']:
+    is_csrf_state_ok = csrf_state == session['csrf_state']
+    if response.status_code == requests.codes.ok and is_csrf_state_ok:
         verify(response.json()["access_token"])
-        id_token = verify(response.json()["id_token"], response.json()["access_token"])
+        id_token = verify(response.json()["id_token"],
+                          response.json()["access_token"])
 
         user = User()
         user.id = id_token["cognito:username"]
@@ -279,6 +259,7 @@ def callback():
             <p>Something went wrong</p>
         {% endblock %}""")
 
+
 @application.errorhandler(401)
 def unauthorized(exception):
     "Unauthorized access route"
@@ -288,14 +269,18 @@ def unauthorized(exception):
             <p>Please login to access this page</p>
         {% endblock %}"""), 401
 
+
 def verify(token, access_token=None):
     """Verify a cognito JWT"""
     # get the key id from the header, locate it in the cognito keys
     # and verify the key
     header = jwt.get_unverified_header(token)
     key = [k for k in JWKS if k["kid"] == header['kid']][0]
-    id_token = jwt.decode(token, key, audience=config.COGNITO_CLIENT_ID, access_token=access_token)
+    id_token = jwt.decode(token, key,
+                          audience=config.COGNITO_CLIENT_ID,
+                          access_token=access_token)
     return id_token
+
 
 if __name__ == "__main__":
     # http://flask.pocoo.org/docs/0.12/errorhandling/#working-with-debuggers
