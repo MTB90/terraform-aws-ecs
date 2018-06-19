c359957b6966b0eaed4337c93f289874a0408fc9
Mateusz Bojanowski <mbojanowski@pgs-soft.com>
2018-06-19 13:43:27 +0200

    Clear flake8 errors.

--- FlaskApp/util.py
+++ FlaskApp/util.py
@@ -1,29 +1,34 @@
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
-# in compliance with the License. A copy of the License is located at
-#
-# https://aws.amazon.com/apache-2-0/
-#
-# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-"""Shared utility helper functions"""
+"""
+Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+https://aws.amazon.com/apache-2-0/
+or in the "license" file accompanying this file.
+This file is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing
+permissions and limitations under the License.
+"""
 import os
 from io import BytesIO
-from PIL import Image, ExifTags
+from PIL import Image
 
 from aws_xray_sdk.core import xray_recorder
 
 EXIF_ORIENTATION = 274  # Magic numbers from http://www.exiv2.org/tags.html
 
+
 def random_hex_bytes(n_bytes):
     """Create a hex encoded string of random bytes"""
     return os.urandom(n_bytes).hex()
 
+
 @xray_recorder.capture()
 def resize_image(file_p, size):
-    """Resize an image to fit within the size, and save to the path directory"""
+    """Resize an image to fit within the size,
+    and save to the path directory"""
     dest_ratio = size[0] / float(size[1])
     try:
         image = Image.open(file_p)
@@ -39,8 +44,8 @@ def resize_image(file_p, size):
             image = image.rotate(270, expand=True)
         elif exif[EXIF_ORIENTATION] == 8:
             image = image.rotate(90, expand=True)
-    except:
-        print("No exif data")
+    except Exception as e:
+        print(f"No exif data {e}")
 
     source_ratio = image.size[0] / float(image.size[1])
 
@@ -49,16 +54,16 @@ def resize_image(file_p, size):
     if image.size < size:
         new_width, new_height = image.size
     elif dest_ratio > source_ratio:
-        new_width = int(image.size[0] * size[1]/float(image.size[1]))
+        new_width = int(image.size[0] * size[1] / float(image.size[1]))
         new_height = size[1]
     else:
         new_width = size[0]
-        new_height = int(image.size[1] * size[0]/float(image.size[0]))
+        new_height = int(image.size[1] * size[0] / float(image.size[0]))
     image = image.resize((new_width, new_height), resample=Image.LANCZOS)
 
     final_image = Image.new("RGBA", size)
-    topleft = (int((size[0]-new_width) / float(2)),
-               int((size[1]-new_height) / float(2)))
+    topleft = (int((size[0] - new_width) / float(2)),
+               int((size[1] - new_height) / float(2)))
     final_image.paste(image, topleft)
     bytes_stream = BytesIO()
     final_image.save(bytes_stream, 'PNG')
