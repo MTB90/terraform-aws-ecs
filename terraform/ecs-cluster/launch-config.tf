6154b8ea9de9b24a3a4b1d6cc89cdc77155fd464
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-01-23 13:51:23 +0100

    Replace string with iam policy document.

--- terraform/ecs-cluster/launch-config.tf
+++ terraform/ecs-cluster/launch-config.tf
@@ -1,11 +1,11 @@
-# Lanunch configuration
+# Launch configuration
 resource "aws_launch_configuration" "ecs_launch_configuration" {
   name = "${format("%s-launch-config", local.name)}"
 
   image_id             = "${var.image_id}"
   instance_type        = "${var.instance_type}"
-  security_groups      = ["${aws_security_group.ecs_launch_configuration_sg.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.ecs_iam_instance_profile.id}"
+  security_groups      = ["${aws_security_group.launch_configuration_sg.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile.id}"
 
   user_data = <<EOF
 #!/bin/bash
@@ -14,38 +14,38 @@ echo ECS_BACKEND_HOST= >> /etc/ecs/ecs.config;
 EOF
 }
 
-resource "aws_iam_role" "ecs_iam_role" {
-  name = "${format("%s-iam-role", local.name)}"
-  tags = "${local.tags}"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-  {
-    "Action": "sts:AssumeRole",
-    "Principal": {
-      "Service": "ec2.amazonaws.com"
-    },
-    "Effect": "Allow",
-    "Sid": ""
-  }]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_iam_role_attachment" {
-  role       = "${aws_iam_role.ecs_iam_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_instance_profile" "ecs_iam_instance_profile" {
-  name = "${format("%s-iam-instance-profile", local.name)}"
-  role = "${aws_iam_role.ecs_iam_role.name}"
-}
-
-resource "aws_security_group" "ecs_launch_configuration_sg" {
+# Security group for launch confuguration
+resource "aws_security_group" "launch_configuration_sg" {
   name   = "${format("%s-launch-config-sg", local.name)}"
   tags   = "${merge(var.tags, map("Name", format("%s-launch-config-sg", local.name)))}"
   vpc_id = "${var.vpc_id}"
+}
+
+# IAM instance profile for launch configuration
+resource "aws_iam_instance_profile" "iam_instance_profile" {
+  name = "${format("%s-iam-instance-profile", local.name)}"
+  role = "${aws_iam_role.iam_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_attachment" {
+  role       = "${aws_iam_role.iam_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role" "iam_role" {
+  name = "${format("%s-iam-role", local.name)}"
+  tags = "${local.tags}"
+
+  assume_role_policy = "${data.aws_iam_policy_document.assume_policy_document.json}"
+}
+
+data "aws_iam_policy_document" "assume_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
