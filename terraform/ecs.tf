583d13af56e056331831a001f61e89202c6964e8
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-01-25 13:41:04 +0100

    Fix: sync ecs file with current implementation

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,59 +1,3 @@
-data "aws_availability_zones" "available" {}
-
-locals {
-  subnets  = "${length(data.aws_availability_zones.available.names)}"
-  vpc_name = "${format("%s-vpc", var.tags["Project"])}"
-}
-
-# VPC
-resource "aws_vpc" "vpc" {
-  tags                 = "${merge(var.tags, map("Module", "vpc", "Name", local.vpc_name))}"
-  cidr_block           = "${var.network_address_space}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
-
-# Create public subnets
-module "public_subnets" {
-  source = "./modules/subnets"
-  tags   = "${merge(var.tags, map("Name", "public"))}"
-
-  vpc_id = "${aws_vpc.vpc.id}"
-  azs    = "${data.aws_availability_zones.available.names}"
-  cidr   = "${var.network_address_space}"
-  shift  = 0
-}
-
-module "igw" {
-  source = "./modules/igw"
-  tags   = "${var.tags}"
-
-  vpc_id         = "${aws_vpc.vpc.id}"
-  route_table_id = "${module.public_subnets.route_table_id}"
-}
-
-# Create app subnets
-module "app_subnets" {
-  source = "./modules/subnets"
-  tags   = "${merge(var.tags, map("Name", "app"))}"
-
-  vpc_id = "${aws_vpc.vpc.id}"
-  azs    = "${data.aws_availability_zones.available.names}"
-  cidr   = "${var.network_address_space}"
-  shift  = "${local.subnets}"
-}
-
-# Create db subnets
-module "db_subnets" {
-  source = "./modules/subnets"
-  tags   = "${merge(var.tags, map("Name", "db"))}"
-
-  vpc_id = "${aws_vpc.vpc.id}"
-  azs    = "${data.aws_availability_zones.available.names}"
-  cidr   = "${var.network_address_space}"
-  shift  = "${2 * local.subnets}"
-}
-
 # ECS resource with launch configuration, auto scaling
 module "ecs_cluster" {
   source = "./modules/ecs-cluster"
@@ -87,17 +31,4 @@ module "ecs_ec2_task_definition" {
   memory           = 128
   workdir          = "/app"
   docker_image_uri = "${var.ecs_docker_image_uri}"
-}
-
-# NAT instance in public
-module "nat_instance" {
-  source = "./modules/nat-instance"
-  tags   = "${var.tags}"
-
-  vpc_id            = "${aws_vpc.vpc.id}"
-  subnet_id         = "${module.public_subnets.subnets[0]}"
-  nat_image_id      = "${var.nat_image_id}"
-  nat_instance_type = "${var.nat_instance_type}"
-  sq_inbound_rule   = "${module.ecs_ec2_launch_configuration.sg_id}"
-  route_table_id    = "${module.app_subnets.route_table_id}"
 }
