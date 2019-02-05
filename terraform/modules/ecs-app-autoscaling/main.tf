d68853ea426590257633eb6c156de6e5bf8073be
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-02-05 16:12:01 +0100

    Removed policy, default policy works fine.

--- terraform/modules/ecs-app-autoscaling/main.tf
+++ terraform/modules/ecs-app-autoscaling/main.tf
@@ -13,7 +13,6 @@ resource "aws_appautoscaling_target" "ecs_app_target" {
 
   resource_id = "${local.resource_id}"
 
-  role_arn           = "${aws_iam_policy.app_autoscaling_service_policy.arn}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
@@ -36,23 +35,4 @@ resource "aws_appautoscaling_policy" "ecs_app_policy" {
   }
 
   depends_on = ["aws_appautoscaling_target.ecs_app_target"]
-}
-
-resource "aws_iam_role" "role_app_autoscaling" {
-  name               = "${format("%s-iam-role", local.name)}"
-  tags               = "${local.tags}"
-  assume_role_policy = "${file("${path.module}/policies/assume-role.json")}"
-}
-
-resource "aws_iam_role_policy_attachment" "role_app_autoscaling_policy_attachment" {
-  role       = "${aws_iam_role.role_app_autoscaling.name}"
-  policy_arn = "${aws_iam_policy.app_autoscaling_service_policy.arn}"
-}
-
-resource "aws_iam_policy" "app_autoscaling_service_policy" {
-  name        = "${format("%s-ec2-container-service", local.name)}"
-  path        = "/"
-  description = "Policy for the app for app autoscaling."
-
-  policy = "${file("${path.module}/policies/app-autoscaling-role.json")}"
 }
