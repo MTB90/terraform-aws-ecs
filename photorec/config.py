b8e3b1838944faa8ba7a5b0556211bc7e7e9cc35
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-02-08 16:29:53 +0100

    Added config file to load env.

---
+++ photorec/config.py
@@ -0,0 +1,8 @@
+import os
+
+AWS_REGION = os.environ['AWS_REGION']
+COGNITO_POOL_ID = os.environ['COGNITO_POOL_ID']
+COGNITO_CLIENT_ID = os.environ['COGNITO_CLIENT_ID']
+COGNITO_CLIENT_SECRET = os.environ['COGNITO_CLIENT_SECRET']
+COGNITO_DOMAIN = os.environ['COGNITO_DOMAIN']
+BASE_URL = os.environ['BASE_URL']
