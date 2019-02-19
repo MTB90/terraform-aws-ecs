90cf6bbe2030d79bd843cd60318da55bbc4b5477
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-02-19 18:23:09 +0100

    Added simple tests for PhotRepo and added boto3 to Pipfile.

--- photorec/repository/photo.py
+++ photorec/repository/photo.py
@@ -1,2 +1,64 @@
+from boto3.dynamodb.conditions import Key
+from typing import Dict, List
+
+
 class PhotoRepo:
-    pass
+    """
+    Repository for photos that encapsulate access to resources.
+    """
+    KEYS = ['nickname', 'tag']
+    FILTERS = {'nickname', 'tag', 'likes'}
+    SORT = {'likes'}
+
+    def __init__(self, db):
+        self._photos = db.Table('photorec-dynamodb-photos')
+
+    def list(self, query: Dict=None, filters: Dict=None) -> List[Dict]:
+        """List all elements that meet query and filter conditions.
+
+        :param query: Query parameters base parameters
+        :param filters: Filters additional parameters for query
+        :return:
+        """
+        query = self._validate_query(query)
+        filters = self._validate_filters(filters)
+        return []
+
+    def _validate_query(self, query: Dict):
+        if query is None:
+            return None
+
+        if len(query) != 1:
+            raise ValueError("Only one parameter is supported for query")
+
+        for key in self.KEYS:
+            if key in query:
+                return Key(key).eq(query[key])
+        raise KeyError(f"Unknown key:{key} for query supported: {self.KEYS}")
+
+    def _validate_filters(self, filters: Dict):
+        if filters is None:
+            return None
+
+        for key, value in filters.items():
+            if '__' not in key:
+                key = key + '__eq'
+
+            key, operator = key.split('__')
+            if key not in self.FILTERS:
+                raise KeyError(f"Unknown key:{key} for filter supported: {self.FILTERS}")
+
+            if operator == 'begins_with':
+                if not isinstance(value, str):
+                    raise ValueError(f"Operator {operator} required string value")
+                continue
+            elif operator == 'between':
+                if not isinstance(value, tuple):
+                    raise ValueError(f"Operator {operator} required tuple value")
+                continue
+            elif operator in {'eq', 'lt', 'gt', 'lte', 'gte'}:
+                if not isinstance(value, int):
+                    raise ValueError(f"Operator {operator} required string value")
+                continue
+
+            raise ValueError(f"Operator {operator} is not supported")
--- tests/repository/test_photo_repo.py
+++ tests/repository/test_photo_repo.py
@@ -1,6 +1,52 @@
+import pytest
+from unittest.mock import Mock
 from repository.photo import PhotoRepo
 
 
-def test_given_photos_repo_when_init_then_object_created():
-    photo_repo = PhotoRepo()
-    assert photo_repo
+def test_given_empty_db_when_no_conditions_then_empty_array():
+    empty_db = Mock()
+    repo = PhotoRepo(empty_db)
+    assert len(repo.list()) == 0
+
+
+def test_given_repo_when_query_with_unknown_params_then_key_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(KeyError):
+        repo.list(query={'unknown': 'unknown'})
+
+
+def test_given_repo_when_query_with_two_params_then_value_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(ValueError):
+        repo.list(query={'nickname': 'nickname', 'tag': 'tag'})
+
+
+def test_given_repo_when_filter_with_unknown_params_then_key_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(KeyError):
+        repo.list(filters={'unknown': 'unknown'})
+
+
+def test_given_repo_when_filter_with_unknown_operator_then_value_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(ValueError):
+        repo.list(filters={'likes__zt': 100})
+
+
+def test_given_repo_when_filter_begins_with_no_str_then_value_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(ValueError):
+        repo.list(filters={'likes__begins_with': 100})
+
+
+def test_given_repo_when_filter_int_operator_with_str_then_value_error():
+    repo = PhotoRepo(Mock())
+    for operator in ['eq', 'lt', 'gt', 'lte', 'gte']:
+        with pytest.raises(ValueError):
+            repo.list(filters={f'likes__{operator}': 'str'})
+
+
+def test_given_repo_when_filter_between_operator_with_int_then_value_error():
+    repo = PhotoRepo(Mock())
+    with pytest.raises(ValueError):
+        repo.list(filters={f'likes__between': 100})
