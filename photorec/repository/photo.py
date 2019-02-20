7bafbcd4f0eee4f876067dfd99446fd078f82d56
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-02-20 17:35:16 +0100

    Split validator from repository.

--- photorec/repository/photo.py
+++ photorec/repository/photo.py
@@ -19,12 +19,83 @@ class UnsupportedFilterOperator(Exception):
     pass
 
 
+class ValidQuery:
+    VALID_KEY = {'nickname', 'tag'}
+
+    def __new__(cls, query: Dict):
+        if len(query) > 1:
+            raise UnsupportedQuery("Only one parameter is supported for query")
+
+        key, value = list(query.items())[0]
+
+        cls._validate_key(key)
+        return cls._cast_value(key, value)
+
+    @classmethod
+    def _validate_key(cls, key):
+        if key not in cls.VALID_KEY:
+            raise UnsupportedQuery(f"Unsupported key:{key} supported: {cls.VALID_KEY}")
+
+    @classmethod
+    def _cast_value(cls, key, value):
+        return Key(key).eq(value)
+
+
+class ValidFilters:
+    VALID_KEY = {'nickname', 'tag', 'likes'}
+    VALID_OPERATOR = {'eq', 'lt', 'gt', 'lte', 'gte', 'between', 'begins_with'}
+
+    def __new__(cls, filters: Dict):
+        valid_filters = []
+        for key, value in filters.items():
+            key, operator = cls._validate_key(key)
+            cls._validate_operator(operator)
+            valid_filters.append(cls._cast_value(key, value, operator))
+
+        return reduce((lambda x, y: x & y), valid_filters)
+
+    @classmethod
+    def _validate_key(cls, key):
+        if '__' not in key:
+            key = key + '__eq'
+
+        key, operator = key.split('__')
+        if key not in cls.VALID_KEY:
+            raise UnsupportedFilter(
+                f"Unsupported filter: {key}, supported: {cls.VALID_KEY}"
+            )
+        return key, operator
+
+    @classmethod
+    def _validate_operator(cls, operator):
+        if operator not in cls.VALID_OPERATOR:
+            raise UnsupportedFilterOperator(
+                f"Unsupported operator: {operator} supported: {cls.VALID_OPERATOR}"
+            )
+
+    @classmethod
+    def _cast_value(cls, key, value, operator):
+        value_type = type(value)
+
+        if value_type == str and operator in {'eq', 'begins_with'}:
+            return getattr(Key(key), operator)(value)
+
+        if value_type == int and operator in {'eq', 'lt', 'gt', 'lte', 'gte'}:
+            return getattr(Key(key), operator)(value)
+
+        if value_type == tuple and operator == 'between':
+            return Key(key).between(*value)
+
+        raise UnsupportedFilterOperator(
+            f"Value: {value} with operator {operator} is not supported"
+        )
+
+
 class PhotoRepo:
     """
     Repository for photos that encapsulate access to resources.
     """
-    QUERY = ['nickname', 'tag']
-    FILTERS = {'nickname', 'tag', 'likes'}
+
     PRIMARY_KEY = {'nickname', 'thumb'}
     REQUIRED_FIELDS = ['nickname', 'thumb', 'photo', 'tag']
 
@@ -63,73 +134,17 @@ class PhotoRepo:
         :param filters: Filters additional parameters for query
         :return:
         """
-        valid_query = self._validate_query(query)
-        valid_filters = self._validate_filters(filters)
         params = {}
 
-        if valid_query is not None:
-            params['KeyConditionExpression'] = valid_query
+        if query is not None:
+            params['KeyConditionExpression'] = ValidQuery(query)
 
-        if valid_filters is not None:
-            params['FilterExpression'] = reduce(
-                (lambda x, y: x & y), valid_filters
-            )
+        if filters is not None:
+            params['FilterExpression'] = ValidFilters(filters)
 
-        if valid_query is None:
+        if query is None:
             response = self._photos.scan(**params)
         else:
             response = self._photos.query(**params)
 
         return response['Items']
-
-    def _validate_query(self, query: Dict):
-        if query is None:
-            return None
-
-        if len(query) != 1:
-            raise UnsupportedQuery("Only one parameter is supported for query")
-
-        for key in self.QUERY:
-            if key in query:
-                return Key(key).eq(query[key])
-        raise UnsupportedQuery(f"Unknown key:{key} for query supported: {self.QUERY}")
-
-    def _validate_filters(self, filters: Dict):
-        if filters is None:
-            return None
-
-        list_filters = []
-        for key, value in filters.items():
-            if '__' not in key:
-                key = key + '__eq'
-
-            key, operator = key.split('__')
-            if key not in self.FILTERS:
-                raise UnsupportedFilter(
-                    f"Unknown filter:{key} for filter supported: {self.FILTERS}"
-                )
-
-            list_filters.append(self._validate_value(key, value, operator))
-
-        return list_filters
-
-    @staticmethod
-    def _validate_value(key, value, operator):
-        valid_operators = {'eq', 'lt', 'gt', 'lte', 'gte', 'between', 'begins_with'}
-        if operator not in valid_operators:
-            raise UnsupportedFilterOperator(f"Operator {operator} is not supported")
-
-        value_type = type(value)
-
-        if value_type == str and operator in {'eq', 'begins_with'}:
-            return getattr(Key(key), operator)(value)
-
-        if value_type == int and operator in {'eq', 'lt', 'gt', 'lte', 'gte'}:
-            return getattr(Key(key), operator)(value)
-
-        if value_type == tuple and operator == 'between':
-            return Key(key).between(*value)
-
-        raise UnsupportedFilterOperator(
-            f"Value: {value} with operator {operator} is not supported"
-        )
