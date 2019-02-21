98e884ab0e377a28b15fe61e4ee33eb84ddaebbf
Matuesz Bojanowski <bojanowski.mateusz@gmail.com>
2019-02-21 14:16:57 +0100

    Added simple Tag repository.

--- photorec/repository/tag.py
+++ photorec/repository/tag.py
@@ -1,2 +1,13 @@
 class TagRepo:
-    pass
+    """
+    Repository for tags that encapsulate access to resources.
+    """
+
+    def __init__(self, db):
+        self._tags = db.Table('photorec-dynamodb-tags')
+
+    def list(self, limit=None):
+        response = self._tags.scan()
+        items = response['Items']
+        items.sort(reverse=True, key=lambda x: x['score'])
+        return items[0:limit]
