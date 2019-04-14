112619f2e6f0bcf9352c83f5f90bed83d5b531cd
Mateusz Bojanowski <bojanowski.mateusz@gmail.com>
2019-04-14 07:47:21 +0200

    Added Flake8 configuration and fix flake8 errors.

--- photorec/validators/nickname.py
+++ photorec/validators/nickname.py
@@ -4,7 +4,7 @@ from errors.domian_errors import ValidationError
 class NicknameError(ValidationError):
     def __init__(self, code=404, message="Error nickname not defined"):
         super().__init__(code=code, message=message)
-        
+
 
 class ValidatorNickname:
     @staticmethod
