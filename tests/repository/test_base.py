21f02663910d941632dbe94f6997220029ea9fad
Mateusz Bojanowski <bojanowski.mateusz@gmail.com>
2019-04-22 15:39:37 +0200

    Added additonal tests for repository base.

--- tests/repository/test_base.py
+++ tests/repository/test_base.py
@@ -1,17 +1,40 @@
-from unittest.mock import Mock
+import pytest
+from unittest.mock import Mock, ANY, call
 from photorec.repository.photo import RepoPhoto
 
 
-def test_given_repository_when_list_with_no_query_filter_then_scan():
+@pytest.fixture()
+def database():
     table = Mock()
     table.scan.return_value = {'Items': []}
+    table.query.return_value = {'Items': []}
 
     database = Mock()
     database.Table.return_value = table
+    return database
 
+
+def test_given_repository_when_list_with_no_query_filter_then_scan(database):
     repo = RepoPhoto(db=database, config=Mock())
     repo.list()
 
+    table = repo.table
     table.scan.assert_called_once_with()
 
 
+def test_given_repository_when_list_with_with_filter_then_scan(database):
+    filters = {'like__between': (1, 100)}
+    repo = RepoPhoto(db=database, config=Mock())
+    repo.list(filters=filters)
+
+    table = repo.table
+    table.scan.assert_called_once()
+
+
+def test_given_repository_when_list_with_with_query_then_scan(database):
+    query = {'nickname': 'nickname'}
+    repo = RepoPhoto(db=database, config=Mock())
+    repo.list(query=query)
+
+    table = repo.table
+    table.query.assert_called_once()
