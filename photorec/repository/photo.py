3d66001685f9eae310b844dc54662a0be4e08979
Mateusz Bojanowski <bojanowski.mateusz@gmail.com>
2019-05-01 20:00:00 +0200

    Added option to query using secondary index for DB.

--- photorec/repository/photo.py
+++ photorec/repository/photo.py
@@ -7,12 +7,12 @@ class RepoPhoto(RepoBase):
     Repository for photos that encapsulate access to resources.
     """
     def __init__(self, db, config):
-        super().__init__(db=db, config=config)
-        self._photos = db.Table(f"{self._config.DATABASE}-photos")
+        secondary_index = {"tag": 'photo-tags', "tag__eq": 'photo-tags'}
+        super().__init__(db=db, config=config, secondary_index=secondary_index)
 
     @property
     def table(self):
-        return self._photos
+        return self._db.Table(f"{self._config.DATABASE}-photos")
 
     def add(self, item: Dict):
         item['likes'] = 0
