56109403a6b50d78603950aec86ba4df987c80a1
Mati <bojanowski.mateusz@gmail.com>
2019-11-27 23:58:26 +0100

    Added terraform infrastructure.

--- infrastructure/terraform/ecs-services/resources.tf
+++ infrastructure/terraform/ecs-services/resources.tf
@@ -1,3 +1,4 @@
+# WEB ECS SERVICE
 module "ecs_web_task_definition" {
   source = "./task-definition"
   tags   = merge(local.tags, map("Name", format("%s-web-ecs-task-def", local.prefix)))
@@ -7,7 +8,7 @@ module "ecs_web_task_definition" {
   workdir          = "/app"
   docker_image_uri = format("%s:latest", data.aws_ecr_repository.web_ecr.repository_url)
 
-  container_definition_file = "${path.root}/task-container/web.tpl"
+  container_definition_file = "${path.root}/task-container/base.tpl"
 
   environments = map(
     "region", var.aws_region,
@@ -30,10 +31,56 @@ module "ecs_web_service" {
 
 module "ecs_web_app_autoscaling" {
   source = "./app-autoscaling"
-  tags   = merge(local.tags, map("Name", format("%s-ecs-app-autoscaling", local.prefix)))
+  tags   = merge(local.tags, map("Name", format("%s-ecs-web-autoscaling", local.prefix)))
 
   cluster_name    = data.aws_ecs_cluster.ecs_cluster.cluster_name
   service_name    = module.ecs_web_service.name
   capacity_limits = local.aws_ecs_container_limits
 }
 
+# API ECS SERVICE
+module "ecs_api_task_definition" {
+  source = "./task-definition"
+  tags   = merge(local.tags, map("Name", format("%s-api-ecs-task-def", local.prefix)))
+
+  cpu_unit         = var.aws_ecs_container_cpu_unit
+  memory           = var.aws_ecs_container_memory
+  workdir          = "/app"
+  docker_image_uri = format("%s:latest", data.aws_ecr_repository.api_ecr.repository_url)
+
+  container_definition_file = "${path.root}/task-container/base.tpl"
+
+  environments = map(
+    "region", var.aws_region,
+    "project", var.aws_project_name,
+    "service", "api",
+    "environment", var.aws_environment_type
+  )
+}
+
+module "ecs_api_service_discovery" {
+  source = "./service-discovery"
+  tags   = merge(local.tags, map("Name", "api"))
+
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+module "ecs_api_service" {
+  source = "./ec2-service"
+  tags   = merge(local.tags, map("Name", format("%s-api-ecs-service", local.prefix)))
+
+  cluster_id            = data.aws_ecs_cluster.ecs_cluster.arn
+  capacity_limits       = local.aws_ecs_container_limits
+  task_definition_arn   = module.ecs_api_task_definition.arn
+  container_name        = module.ecs_api_task_definition.container_name
+  service_discovery_arn = module.ecs_api_service_discovery.arn
+}
+
+module "ecs_api_app_autoscaling" {
+  source = "./app-autoscaling"
+  tags   = merge(local.tags, map("Name", format("%s-ecs-api-autoscaling", local.prefix)))
+
+  cluster_name    = data.aws_ecs_cluster.ecs_cluster.cluster_name
+  service_name    = module.ecs_api_service.name
+  capacity_limits = local.aws_ecs_container_limits
+}
